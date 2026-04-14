d901af6aa91f37d06d41559a59467679c4b8fdb3
Dmitry Zhuravlev <zhdm.dev@gmail.com>
2026-04-14 11:26:00 -0400

    feat(terraform): add fresh-project API bootstrap and dedicated GKE node service account

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,9 +13,50 @@ provider "google" {
   region  = var.region
 }
 
+locals {
+  # Minimum APIs required for project bootstrap and GKE deployment.
+  required_apis = toset([
+    "serviceusage.googleapis.com",
+    "compute.googleapis.com",
+    "container.googleapis.com",
+    "iam.googleapis.com",
+    "monitoring.googleapis.com",
+    "logging.googleapis.com",
+  ])
+
+  # Least-privilege baseline recommended by GKE for node service accounts.
+  gke_node_sa_roles = toset([
+    "roles/container.defaultNodeServiceAccount",
+  ])
+}
+
+resource "google_project_service" "required" {
+  for_each = local.required_apis
+
+  project            = var.project_id
+  service            = each.value
+  disable_on_destroy = false
+}
+
+resource "google_service_account" "gke_nodes" {
+  account_id   = "online-shop-gke-nodes"
+  display_name = "online-shop GKE nodes"
+  description  = "Node service account for the online-shop GKE cluster"
+}
+
+resource "google_project_iam_member" "gke_nodes" {
+  for_each = local.gke_node_sa_roles
+
+  project = var.project_id
+  role    = each.value
+  member  = "serviceAccount:${google_service_account.gke_nodes.email}"
+}
+
 resource "google_compute_network" "vpc" {
   name                    = var.network_name
   auto_create_subnetworks = false
+
+  depends_on = [google_project_service.required]
 }
 
 resource "google_compute_subnetwork" "subnet" {
@@ -23,6 +64,8 @@ resource "google_compute_subnetwork" "subnet" {
   region        = var.region
   network       = google_compute_network.vpc.id
   ip_cidr_range = var.subnet_cidr
+
+  depends_on = [google_project_service.required]
 }
 
 resource "google_container_cluster" "gke" {
@@ -44,6 +87,8 @@ resource "google_container_cluster" "gke" {
   release_channel {
     channel = "REGULAR"
   }
+
+  depends_on = [google_project_service.required]
 }
 
 locals {
@@ -71,14 +116,20 @@ resource "google_container_node_pool" "pools" {
   location = var.region
 
   node_config {
-    machine_type = each.value.machine
-    labels       = each.value.labels
-    oauth_scopes = ["https://www.googleapis.com/auth/cloud-platform"]
+    service_account = google_service_account.gke_nodes.email
+    machine_type    = each.value.machine
+    labels          = each.value.labels
+    oauth_scopes    = ["https://www.googleapis.com/auth/cloud-platform"]
   }
 
   autoscaling {
     min_node_count = 0
     max_node_count = 5
   }
+
+  depends_on = [
+    google_project_service.required,
+    google_project_iam_member.gke_nodes,
+  ]
 }
 
